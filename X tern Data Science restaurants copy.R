bb4a5e6040ed0ef92cca9b8c394c9a169694e11f
Y-Atlas2 <100858111+Y-Atlas2@users.noreply.github.com>
2022-10-18 11:51:15 -0400

    Add files via upload

---
+++ X tern Data Science restaurants copy.R
@@ -0,0 +1,3 @@
+google_places(search_string = 'restaurant',
+              location=c(39.791000, -86.148003), radius=15411,
+              key="(My key here)")
---
+++ X-tern project things to do copy.R
@@ -0,0 +1,3 @@
+google_places(search_string = 'Things to do',
+              location=c(39.791000, -86.148003), radius=15411,
+              key="AIzaSyDVO6BpcwUQTwoR7_A7hwJz8i4IV90Kg7o")
